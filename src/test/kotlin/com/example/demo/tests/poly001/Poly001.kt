b95a95dd63be9cca4359e2f47c3e16dd657ff56e
sebastian.schmidt <bastman77@gmail.com>
2019-12-12 12:33:40 +0100

    initial

--- src/test/kotlin/com/example/demo/tests/poly001/Poly001.kt
+++ src/test/kotlin/com/example/demo/tests/poly001/Poly001.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeName
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.amshove.kluent.`should be instance of`
-import org.junit.jupiter.api.Test
+import org.amshove.kluent.shouldEqual
 import org.junit.jupiter.api.TestFactory
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.Instant
@@ -17,20 +17,51 @@ class Poly001 {
 
     @TestFactory
     fun foo() = testFactory {
-        test("a") {
-            val a=ADto(fId = UUID.randomUUID(), createdAt = Instant.now(), a=100)
+        test("a - works with jackson 2.10.1") {
+            val a = ADto(
+                    myId = UUID.fromString("405334a1-f2b1-492a-b782-dc921572e0d8"),
+                    createdAt = Instant.parse("2019-12-12T10:52:23.372952Z"),
+                    a = 100
+            )
             val aJson = JSON.writeValueAsString(a)
             println("json: $aJson")
-            val aReloaded:ADto = JSON.readValue(aJson)
+            val aReloaded: ADto = JSON.readValue(aJson)
             aReloaded `should be instance of` FDto::class
             aReloaded `should be instance of` ADto::class
+            aReloaded.createdAt shouldEqual a.createdAt
+            aReloaded.myId shouldEqual a.myId
+            aReloaded shouldEqual a
+
+            val fReloaded: FDto = JSON.readValue(aJson)
+            fReloaded `should be instance of` FDto::class
+            fReloaded `should be instance of` ADto::class
+            fReloaded.createdAt shouldEqual a.createdAt
+            fReloaded.myId shouldEqual a.myId
+            fReloaded shouldEqual a
+            fReloaded shouldEqual aReloaded
         }
-        test("b") {
-            val b=BDto(fId = UUID.randomUUID(), createdAt = Instant.now(), b=true)
+        test("b - works with jackson 2.10.1") {
+            val b = BDto(
+                    myId = UUID.fromString("405334a1-f2b1-492a-b782-dc921572e0d8"),
+                    createdAt = Instant.parse("2019-12-12T10:52:23.372952Z"),
+                    b = true
+            )
             val bJson = JSON.writeValueAsString(b)
-            val bReloaded:FDto = JSON.readValue(bJson)
+            val bReloaded: FDto = JSON.readValue(bJson)
             bReloaded `should be instance of` FDto::class
-            bReloaded `should be instance of` ADto::class
+            bReloaded `should be instance of` BDto::class
+            bReloaded.createdAt shouldEqual b.createdAt
+            bReloaded.myId shouldEqual b.myId
+            bReloaded shouldEqual b
+
+            val fReloaded: FDto = JSON.readValue(bJson)
+            fReloaded `should be instance of` FDto::class
+            fReloaded `should be instance of` BDto::class
+            fReloaded.createdAt shouldEqual b.createdAt
+            fReloaded.myId shouldEqual b.myId
+
+            fReloaded shouldEqual b
+            fReloaded shouldEqual bReloaded
         }
 
     }
@@ -41,28 +72,28 @@ private val JSON = Jackson.defaultMapper()
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 sealed class FDto {
-    abstract val fId: UUID
+    abstract val myId: UUID
     abstract val createdAt: Instant
 }
 
 @JsonTypeName(ADto.JSON_TYPE)
 data class ADto(
-        override val fId: UUID,
+        override val myId: UUID,
         override val createdAt: Instant,
-        val a:Int
-):FDto() {
+        val a: Int
+) : FDto() {
     companion object {
-        const val JSON_TYPE="ADto"
+        const val JSON_TYPE = "ADto"
     }
 }
 
 @JsonTypeName(BDto.JSON_TYPE)
 data class BDto(
-        override val fId: UUID,
+        override val myId: UUID,
         override val createdAt: Instant,
-        val b:Boolean
-):FDto() {
+        val b: Boolean
+) : FDto() {
     companion object {
-        const val JSON_TYPE="BDto"
+        const val JSON_TYPE = "BDto"
     }
 }
