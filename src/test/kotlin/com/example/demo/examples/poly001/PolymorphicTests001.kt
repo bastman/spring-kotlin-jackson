e0c4c68fb9ffb18d4915578227f0e74698f0540e
sebastian.schmidt <bastman77@gmail.com>
2020-02-26 13:59:04 +0100

    add examples

--- src/test/kotlin/com/example/demo/examples/poly001/PolymorphicTests001.kt
+++ src/test/kotlin/com/example/demo/examples/poly001/PolymorphicTests001.kt
@@ -111,6 +111,52 @@ class PolymorphicTests001 {
         decoded4 `should be instance of` Unknown::class
     }
 
+    @Test
+    fun requestB() {
+        val txt = """
+            
+            {
+              "eventId": "2228400b-995d-41cb-9c28-fa7bc63c88a7",
+              "eventTime": "2020-02-26T09:33:28.775183Z",
+              "eventType": "the-event-B",
+              "data": {
+                "b": "bar"
+              }
+            }
+            
+               """.trimIndent()
+
+        // decode as Unknown
+        val decoded1: Unknown = JSON.readValue(txt)
+        println(decoded1)
+        println(decoded1.eventType)
+        decoded1 `should be instance of` IEvent::class
+        decoded1 `should not be instance of` IEventPolymorphic::class
+        decoded1 `should be instance of` Unknown::class
+        decoded1 `should not be instance of` EventA::class
+        decoded1 `should not be instance of` EventB::class
+
+        // decode as IEventPolymorphic (EventB)
+        val decoded2: IEventPolymorphic = JSON.readValue(txt)
+        println(decoded2)
+        println(decoded2.eventType)
+        decoded2 `should be instance of` IEvent::class
+        decoded2 `should be instance of` IEventPolymorphic::class
+        decoded2 `should be instance of` EventB::class
+        decoded2 `should not be instance of` EventA::class
+        decoded2 `should not be instance of` Unknown::class
+
+        // decoded1 (Unknown) convertTo Event (RequestB)
+        val decoded3: IEventPolymorphic = JSON.convertValue(decoded1)
+        println(decoded3)
+        println(decoded3.eventType)
+        decoded3 `should be instance of` IEvent::class
+        decoded3 `should be instance of` IEventPolymorphic::class
+        decoded3 `should be instance of` EventB::class
+        decoded3 `should not be instance of` EventA::class
+        decoded3 `should not be instance of` Unknown::class
+    }
+
 
 }
 
